cbd2fdef6b18c1bc7a321b62380956724fabd6e1
Kacper <poneciak@gmail.com>
2022-04-12 09:09:08 +0200

    Created PageView system
    added methods to add new view and get all views
    also deleted pageviewscontroller class bcs its not needed at all
    **DIDNT TEST**

--- PHP/Classes/pageviews.class.php
+++ PHP/Classes/pageviews.class.php
@@ -2,4 +2,34 @@
 require_once "DBh.class.php";
 class PageViews extends DBh
 {
+    public function getViews(): int
+    {
+        $stmt = $this->connect()->prepare("SELECT COUNT(ID) FROM odwiedziny;");
+        if (!$stmt->execute()) {
+            $stmt = null;
+            header("location: index.php?error=StmtError");
+            exit();
+        }
+        $count = $stmt->fetchAll(PDO::FETCH_ASSOC)[0][0];
+        $stmt = null;
+        return $count;
+    }
+    public function addView()
+    {
+        $stmt = $this->connect()->prepare("INSERT INTO odwiedziny(IP_adress) VALUES(?);");
+        $stmt->execute([$this->getIP()]);
+        $stmt = null;
+    }
+
+    private function getIP(): string
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 }
--- PHP/Embeds/pageviews-count.embed.php
+++ PHP/Embeds/pageviews-count.embed.php
@@ -1,4 +1,9 @@
 <?php
 
 $dir = dirname(__FILE__);
-require_once("$dir/../Classes/pageviews.controller.php");
+require_once("$dir/../Classes/pageviews.class.php");
+$PV = new PageViews;
+
+echo "Views: {$PV->getViews()}</br>";
+$PV->addView();
+echo "Views: {$PV->getViews()}";
