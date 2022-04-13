22da815f95952b955d7b9373714f7fcbbc0a1a8f
Kacper <poneciak@gmail.com>
2022-04-13 20:17:15 +0200

    Created needed classes
    created offer class
    offer controller class
    and offer struct class

---
+++ PHP/Classes/Structs/offer.struct.php
@@ -0,0 +1,19 @@
+<?php
+
+class Offer{
+    public $id = '`ID`';
+    public $model = '`model_id`';
+    public $cena = '`cena`';
+    public $rok = '`rok_produkcji_id`';
+    public $przebieg = '`przebieg`';
+    public $data = '`data`';
+    public $moc = '`moc_silnika`';
+    public $paliwo = '`paliwo_id`';
+    public $czy_manual = '`czy_manual`';
+    public $kraj = '`kraj_pochodzenia_id`';
+    public $kolor = '`kolor_id`';
+    public $czy_wypadkowy = '`czy_wypadkowy`';
+    public $zdjecie = '`zdjecie`';
+
+    public $mask = 0b0000000000000;
+}
---
+++ PHP/Classes/offers.class.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "DBh.class.php";
+require_once "Structs/offer.struct.php";
+
+class Offers extends DBh
+{
+    protected function stack(int $offers_nr, int $stack_nr, Offer $offer): array
+    {
+    }
+
+    protected function single(int $ID): Offer
+    {
+    }
+
+    protected function add(Offer $offer)
+    {
+    }
+
+    protected function delete(int $ID)
+    {
+    }
+}
