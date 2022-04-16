86644b4af468094dc0d4e5132764f2c49ccc2e6a
karol <mineswek@gmail.com>
2022-04-17 00:09:29 +0200

    Created footer logo
    I've created footer logo with grid template on whole footer

--- Modules/footer.module.php
+++ Modules/footer.module.php
@@ -1,4 +1,9 @@
-<link href="../Css/footer.module.styles.css" rel="stylesheet">
+<link href="../../Css/footer.module.styles.css" rel="stylesheet">
 <footer>
-
+    <div id="footer-grid">
+        <div id="footer-logo">
+            <div id="footer-logo-image"></div>
+            Nazwa Firmy
+        </div>
+    </div>
 </footer>
