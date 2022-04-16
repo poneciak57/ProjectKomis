b3b8fc80b9b75d213e2c13b41cc27b124b82308b
karol <mineswek@gmail.com>
2022-04-16 19:54:36 +0200

    Finishing touches
    I finished navbar module and tested it on testSite that I created also in modules

---
+++ Modules/navbar.module.php
@@ -0,0 +1,20 @@
+<link href="../Css/navbar.module.styles.css" rel="stylesheet">
+<nav id="navbar"> 
+    <div id="navbar-logo">
+        <div id="navbar-logo-image"></div>
+        Komis Samochodowy<br>Nazwa Firmy
+    </div>
+
+    <div id="navbar-pages">
+        <div id="navbar-favorites" onclick="" class="clickable-buttons">
+            <img src="../Sources/heart-icon-white.svg" alt="heart icon" id="navbar-favorites-image">
+            Ulubione
+        </div>
+
+        <div id="navbar-myprofile" onclick="" class="clickable-buttons">
+            <img src="../Sources/user-icon-white.svg" alt="user icon" id="navbar-myprofile-image">
+            Mój profil
+            <img src="../Sources/arrow-icon-white.svg" alt="arrow icon"  id="navbar-myprofile-arrow">
+        </div>  
+    </div>
+</nav>
