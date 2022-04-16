54d4b60a6de0ceec75bdece1d9bec6e7a4754bd7
karol <mineswek@gmail.com>
2022-04-17 00:44:07 +0200

    Finished footer
    I've finished footer module.
    To do:
    - Add href links to every <a> tag.
    - Add logo icon and change company name.

--- Modules/footer.module.php
+++ Modules/footer.module.php
@@ -5,5 +5,27 @@
             <div id="footer-logo-image"></div>
             Nazwa Firmy
         </div>
+        <h1>Pytania</h1>
+        <h1>Klient</h1>
+        <h1>Informacje</h1>
+        <h1>Znajdziesz nas na</h1>
+        <p></p>
+        <p>Zadzwoń do nas: +48 111 222 333</p>
+        <a href="">Moje Konto</a>
+        <a href="../Documents/Polityki prywatności w sklepie internetowym- wzór.pdf">Polityka prywatności</a>
+        <div id="footer-finduson-images">
+            <a href="" class="footer-finduson-image"><?php include("../../Sources/instagram-icon.svg")?></a>
+            <a href="" class="footer-finduson-image"><?php include("../../Sources/github-icon.svg")?></a>
+            <a href="" class="footer-finduson-image"><?php include("../../Sources/facebook-icon.svg")?></a>
+            <a href="" class="footer-finduson-image"><?php include("../../Sources/twitter-icon.svg")?></a>
+        </div>
+        <p></p>
+        <a href="">Kontakt</a>
+        <a href="">Oferta</a>
+        <a href="">Dane firmy</a>
+    </div>
+    <div id="footer-bottom-line">
+        <hr>
+        <p>© Komis samochodowy</p>
     </div>
 </footer>
