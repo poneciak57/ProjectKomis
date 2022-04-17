ce769a0890b8af718b3da8742f2e220e87e775de
MineSwek <85877644+MineSwek@users.noreply.github.com>
2022-04-17 12:48:34 +0200

    Finished Footer module (#11)
    Footer module and images on navbar module

--- Modules/navbar.module.php
+++ Modules/navbar.module.php
@@ -1,4 +1,4 @@
-<link href="../Css/navbar.module.styles.css" rel="stylesheet">
+<link href="../../Css/navbar.module.styles.css" rel="stylesheet">
 <nav id="navbar"> 
     <div id="navbar-logo">
         <div id="navbar-logo-image"></div>
@@ -7,14 +7,14 @@
 
     <div id="navbar-pages">
         <div id="navbar-favorites" onclick="" class="clickable-buttons">
-            <img src="../Sources/heart-icon-white.svg" alt="heart icon" id="navbar-favorites-image">
+            <img src="../../Sources/heart-icon-white.svg" alt="heart icon" id="navbar-favorites-image">
             Ulubione
         </div>
 
         <div id="navbar-myprofile" onclick="" class="clickable-buttons">
-            <img src="../Sources/user-icon-white.svg" alt="user icon" id="navbar-myprofile-image">
+            <img src="../../Sources/user-icon-white.svg" alt="user icon" id="navbar-myprofile-image">
             Mój profil
-            <img src="../Sources/arrow-icon-white.svg" alt="arrow icon"  id="navbar-myprofile-arrow">
+            <img src="../../Sources/arrow-icon-white.svg" alt="arrow icon"  id="navbar-myprofile-arrow">
         </div>  
     </div>
 </nav>
