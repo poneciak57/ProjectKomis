87fe13d33dc2010229a1c359c8fcff9ee631e3be
MineSwek <85877644+MineSwek@users.noreply.github.com>
2022-04-19 17:23:42 +0200

    Finished main html and css of offers (#13)
    I've finished main html and css of the offers page
    - Added arrow icon

---
+++ Modules/car-offer.module.php
@@ -0,0 +1,24 @@
+<div class="carOffer">
+    <div class="carOffer-main">
+        <div class="carOffer-img">
+            <div class="image"></div>
+        </div>
+        <div class="carOffer-content">
+            <div class="carOffer-content-left">
+                <div class="carOffer-content-left-top">
+                    <h2 class="carOffer-content-title">Przykładowy tytuł oferty samochodu</h2>
+                    <p class="carOffer-content-desc1">2017 / Diesel / 140 123 km</p>
+                    <p class="carOffer-content-desc2">Skrzynia biegów: <b>Manualna</b></p>
+                </div>
+                <div class="carOffer-content-left-bottom">
+                    <p class="carOffer-content-date">02.02.2022</p>
+                </div>
+            </div>
+            <div class="carOffer-content-right">
+                <img src="/ProjectKomis/Sources/heart-icon.svg" alt="heart icon" onclick="">
+                <div class="carOffer-content-price">12 200zł</div>
+            </div>
+        </div>
+    </div>
+    <hr class="carOffer-line">
+</div>
