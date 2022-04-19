cd5fa6ffcf50968ddac5e2313d5dd12373bb483e
karol <mineswek@gmail.com>
2022-04-19 02:49:47 +0200

    Added car-offer module
    I've added car-offer module that can be used to display car offer.
    - Small changes in variable styles which led to changes in footer module and offers page
    - Added new icon heart-icon-red which will be used later in javascript
    - #carOffer-content-title - title of the offer loaded from database
    - #carOffer-content-desc1 - year / fuel / mileage - loaded from database
    - #carOffer-content-desc2 - Skrzynia biegów: gearbox - loaded from database
    - #carOffer-content-price - price loaded from database
    - #carOffer-content-date  - date of the offer loaded from database

---
+++ Modules/car-offer.module.php
@@ -0,0 +1,21 @@
+<div id="carOffer-main">
+    <div id="carOffer-img">
+        <div id="image"></div>
+    </div>
+    <div id="carOffer-content">
+        <div id="carOffer-content-left">
+            <div id="carOffer-content-left-top">
+                <h2 id="carOffer-content-title">Przykładowy tytuł oferty samochodu</h2>
+                <p id="carOffer-content-desc1">2017 / Diesel / 140 123 km</p>
+                <p id="carOffer-content-desc2">Skrzynia biegów: <b>Manualna</b></p>
+            </div>
+            <div id="carOffer-content-left-bottom">
+                <p id="carOffer-content-date">02.02.2022</p>
+            </div>
+        </div>
+        <div id="carOffer-content-right">
+            <img src="/ProjectKomis/Sources/heart-icon.svg" alt="heart icon" onclick="">
+            <div id="carOffer-content-price">12 200zł</div>
+        </div>
+    </div>
+</div>
